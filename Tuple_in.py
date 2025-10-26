bd633fc7884dd516e2c7b240a4030035217aadfc
JayKalambate <139536749+JayKalambate@users.noreply.github.com>
2025-10-26 21:21:24 +0530

    This is tuple Practical
    This is an Tuple practical and also performed some functions on the tuple.

---
+++ Tuple_in.py
@@ -0,0 +1,19 @@
+# Creating a tupple
+tup = ()
+print(tup)
+
+# Tuple using integer values
+tup1 = (1,3,4,5,6)
+print(tup1)
+
+#Tuple using String values
+tup2 = ('Jay','Aliens','Jaadu')
+print(tup2)
+
+# Tuple Using built-in function
+li = tuple("Jay")
+print(li)
+
+# Converting list into tuple 
+list = [1,2,34,67,97]
+print(tuple(list))
